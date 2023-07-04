293a22a3af73df7f3da0448c0575ad5ddcdb545a
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2023-07-04 15:24:33 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
 function setup(){
     canvas = createCanvas(600,400)
+    video = createCapture(VIDEO);
+    video.size(600,400);
+    canvas.position(700,130);
+    poseNet = ml5.poseNet(video,modelLoaded);
+    poseNet.on("pose",gotResults)
+}
+function modelLoaded(){
+    console.log("LOADED!");
+}
+function gotResults(results){
+    if (results.length > 0){
+        console.log(results);
+    }
 }
