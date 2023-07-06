aa491654a6452cd271be822c5a3c964244bddb3a
Cat Meow <110690475+catMeooww@users.noreply.github.com>
2023-07-06 15:59:12 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,22 @@
+diference = 0;
+rightWristX = 0;
+leftWristX = 0;
+noseX = 0;
+noseY = 0;
+texto = "Creeper"
 function setup(){
     canvas = createCanvas(600,400)
     video = createCapture(VIDEO);
     video.size(600,400);
-    canvas.position(700,130);
+    canvas.position(700,150);
     poseNet = ml5.poseNet(video,modelLoaded);
     poseNet.on("pose",gotResults)
+}
+function draw(){
+    background(95,158,160);
+    textSize(diference);
+    fill(0,255,0);
+    text(texto,noseX - 50, noseY);
 }
 function modelLoaded(){
     console.log("LOADED!");
@@ -12,5 +24,14 @@ function modelLoaded(){
 function gotResults(results){
     if (results.length > 0){
         console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        diference = floor(leftWristX - rightWristX);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        document.getElementById("FontWidth").innerHTML = "Fonte: " + diference;
     }
+}
+function newText(){
+    texto = document.getElementById("text").value;
 }
